13ee9f6b64e541b1a8b71e3d5872f12f411781d3
Gabriel de Souza Alves <doutorvirus@hotmail.com>
2021-11-17 15:46:48 -0300

    adicionado leitura do resultado e filtro wookiee

--- SwapiConnection.py
+++ SwapiConnection.py
@@ -3,14 +3,16 @@
 
 class SwapiConection:
     import requests
+    import json
 
     def __init__(self):
         self.conected = False
 
-    def get_request(self, p_resource = None, p_id = None):
+    def get_request(self, p_resource = None, p_id = None, p_wookiee = None):
         self.url = self.build_url(p_resource, p_id)
-        self.response = self.requests.get(self.url)
-        print(self.response.json())
+        self.payload = {'format': p_wookiee}
+        self.response = self.requests.get(self.url, params=self.payload)
+        return self.response.text
 
     def build_url(self, p_resource, p_id):
         self.base_api_url = 'http://swapi.dev/api'
@@ -24,30 +26,39 @@ class SwapiConection:
 
         return self.result_url 
 
+    def read_result(self, p_result):
+        self.result_dict = self.json.loads(p_result)
+        for key in self.result_dict:
+            print(f'{key} - {self.result_dict[key]}')
+
 
 def main():
     import sys
 
     conection = SwapiConection()
+    result_conection = None
+
     if len(sys.argv) == 1:
-        conection.get_request()
-        return
-    if len(sys.argv) == 2:
+        result_conection = conection.get_request()
+    elif len(sys.argv) == 2:
         resource_value = sys.argv[1]
-        conection.get_request(resource_value)
-        return    
-    if len(sys.argv) == 3:
+        result_conection = conection.get_request(resource_value)    
+    elif len(sys.argv) == 3 or len(sys.argv) == 4:
         resource_value = sys.argv[1]
         id_value = sys.argv[2]
         try:
             id_value_int = int(id_value)
         except ValueError as e:
             print("Entre com um valor inteiro positivo como argumento do Id.")
-            return
-        conection.get_request(resource_value, id_value_int)
-        return 
+        if len(sys.argv) == 4:
+            wookiee_value = sys.argv[3]
+        else :
+            wookiee_value = None
+        result_conection = conection.get_request(resource_value, id_value_int, wookiee_value) 
+    else :
+        print("Execute o programa como no exemplo: 'python SwapiConnection.py' ou adicione o recurso ou até mesmo o Id do recurso. Para mais informações sobre recursos e Ids, acesse https://swapi.dev/")
 
-    print("Execute o programa como no exemplo: 'python SwapiConnection.py' ou adicione o recurso ou até mesmo o Id do recurso. Para mais informações sobre recursos e Ids, acesse https://swapi.dev/")
+    conection.read_result(result_conection)
 
 if __name__ == "__main__": 
     main()        
